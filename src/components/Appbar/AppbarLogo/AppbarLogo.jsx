62c5c3623b3cdbd991062db83cbc34343ca66de4
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-03-05 07:24:26 +0200

    update Logo

--- src/components/Appbar/AppbarLogo/AppbarLogo.jsx
+++ src/components/Appbar/AppbarLogo/AppbarLogo.jsx
@@ -1,9 +1,13 @@
+import { IoGameController } from 'react-icons/io5';
 import styles from './AppbarLogo.module.css';
 
 function AppbarLogo() {
   return (
     <div className={styles.logo}>
-      <span>Gaming</span> store
+      <span>
+        <IoGameController size={20} />
+      </span>{' '}
+      g-store
     </div>
   );
 }
