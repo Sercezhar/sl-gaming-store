557aaf62ef82622a8e66b1eda925423c15ed2cca
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-04-20 22:38:21 +0300

    update paths

--- src/components/Appbar/AppbarActions/data/flags.js
+++ src/components/Appbar/AppbarActions/data/flags.js
@@ -1,6 +1,6 @@
 const flags = [
-  { name: 'English', icon: 'src/assets/icons.svg#icon-gb', tag: 'en' },
-  { name: 'Українська', icon: 'src/assets/icons.svg#icon-ua', tag: 'ua' },
+  { name: 'English', icon: '/assets/icons.svg#icon-gb', tag: 'en' },
+  { name: 'Українська', icon: '/assets/icons.svg#icon-ua', tag: 'ua' },
 ];
 
 export default flags;
