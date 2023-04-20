5a78cd8656f6c37c54fe208c82d77ffbb16ee2f5
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-04-20 22:18:56 +0300

    setup gh-pages

--- src/components/ImageSlider/images.js
+++ src/components/ImageSlider/images.js
@@ -1,22 +1,22 @@
 const images = [
   {
-    url: 'src/assets/images/releases/dead-space-remake.jpg',
+    url: 'assets/images/releases/dead-space-remake.jpg',
     alt: 'dead-space-remake',
   },
   {
-    url: 'src/assets/images/releases/hogwarts-legacy.jpg',
+    url: 'assets/images/releases/hogwarts-legacy.jpg',
     alt: 'hogwarts-legacy',
   },
   {
-    url: 'src/assets/images/releases/wo-long-fallen-dynasty.jpg',
+    url: 'assets/images/releases/wo-long-fallen-dynasty.jpg',
     alt: 'wo-long-fallen-dynasty',
   },
   {
-    url: 'src/assets/images/releases/resident-evil-4-remake.jpg',
+    url: 'assets/images/releases/resident-evil-4-remake.jpg',
     alt: 'resident-evil-4-remake',
   },
   {
-    url: 'src/assets/images/releases/hollow-knight-silksong.jpg',
+    url: 'assets/images/releases/hollow-knight-silksong.jpg',
     alt: 'hollow-knight-silksong',
   },
 ];
