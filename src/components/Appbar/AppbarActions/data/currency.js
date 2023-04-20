4a617e924cdb5c6f3496768100f31f7822e917b3
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-04-20 22:56:21 +0300

    update paths

--- src/components/Appbar/AppbarActions/data/currency.js
+++ src/components/Appbar/AppbarActions/data/currency.js
@@ -1,6 +1,6 @@
 const currency = [
-  { name: 'USD', icon: '/assets/icons.svg#icon-usd' },
-  { name: 'UAH', icon: '/assets/icons.svg#icon-uah' },
+  { name: 'USD', icon: './assets/icons.svg#icon-usd' },
+  { name: 'UAH', icon: './assets/icons.svg#icon-uah' },
 ];
 
 export default currency;
