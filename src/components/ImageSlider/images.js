25ef9a73415d64307dce44133cd3833687eb9000
Oleksandr Havryliuk <deolis.ua@gmail.com>
2023-04-20 22:31:01 +0300

    update images.js

--- src/components/ImageSlider/images.js
+++ src/components/ImageSlider/images.js
@@ -1,22 +1,22 @@
 const images = [
   {
-    url: 'assets/images/releases/dead-space-remake.jpg',
+    url: '/assets/images/releases/dead-space-remake.jpg',
     alt: 'dead-space-remake',
   },
   {
-    url: 'assets/images/releases/hogwarts-legacy.jpg',
+    url: '/assets/images/releases/hogwarts-legacy.jpg',
     alt: 'hogwarts-legacy',
   },
   {
-    url: 'assets/images/releases/wo-long-fallen-dynasty.jpg',
+    url: '/assets/images/releases/wo-long-fallen-dynasty.jpg',
     alt: 'wo-long-fallen-dynasty',
   },
   {
-    url: 'assets/images/releases/resident-evil-4-remake.jpg',
+    url: '/assets/images/releases/resident-evil-4-remake.jpg',
     alt: 'resident-evil-4-remake',
   },
   {
-    url: 'assets/images/releases/hollow-knight-silksong.jpg',
+    url: '/assets/images/releases/hollow-knight-silksong.jpg',
     alt: 'hollow-knight-silksong',
   },
 ];
